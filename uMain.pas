ff99f91ec38ba7f1949ff6bc3ccf23c75c88b6a8
kutsoff <kutsoff@gmail.com>
2011-04-27 09:12:44 +0400

    First commit
    Signed-off-by: kutsoff <kutsoff@gmail.com>

---
+++ uMain.pas
@@ -0,0 +1,39 @@
+unit uMain;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, AdvSmoothListBox, DBAdvSmoothListBox, dxRibbonForm, dxSkinsCore,
+  dxSkinOffice2010Black, cxLookAndFeels, dxSkinsForm, DB, ZAbstractRODataset,
+  ZAbstractDataset, ZDataset, ZConnection;
+
+type
+  TForm4 = class(TdxRibbonForm)
+    DBAdvSmoothListBox1: TDBAdvSmoothListBox;
+    dxSkinController1: TdxSkinController;
+    ZConnection1: TZConnection;
+    ZQuery1: TZQuery;
+    DataSource1: TDataSource;
+    procedure DataSource1DataChange(Sender: TObject; Field: TField);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form4: TForm4;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm4.DataSource1DataChange(Sender: TObject; Field: TField);
+begin
+  if DataSource1.DataSet.Active then  
+  DBAdvSmoothListBox1.Footer.Caption := DataSource1.DataSet.FieldByName('alias').AsString + ' ' +
+    DataSource1.DataSet.FieldByName('mac').AsString;
+end;
+
+end.
